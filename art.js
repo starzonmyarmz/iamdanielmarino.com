b6457ef7abca46d6d80612c2019ed348809bde0a
starzonmyarmz <starzonmyarmz@users.noreply.github.com>
2024-09-19 15:49:48 +0000

    deploy: 81480579ae0a730ae11780744b3d7b9191afb83e

--- art.js
+++ art.js
@@ -1,8 +1,17 @@
+// Takes two integers and creates a 2D array
+
 function create2DArray(rows, cols) {
   const arr = []
   for (let i = 0; i < cols; i++) arr.push(Array(rows).fill(0))
   return arr
 }
+
+
+
+// The first parameter accepts an object with either one or two values:
+// - With a single value, it creates a grid with the same number of rows and columns.
+// - With two values, it creates a grid with different rows and columns.
+// The second parameter is a function that determines how each tile is repeated or rendered.
 
 function tile(options, f) {
   const {
@@ -20,9 +29,17 @@ function tile(options, f) {
   }
 }
 
+
+
+// Redraw the canvas when clicking on it
+
 document.addEventListener('click', ({ target }) => {
   if (target.matches('canvas')) redraw()
 })
+
+
+
+// Save an image of the current canvas when pressing "s"
 
 document.addEventListener('keydown', ({ key }) => {
   if (key === 's') saveCanvas(`${document.querySelector('title').innerText}_${Date.now()}`, 'webp')
