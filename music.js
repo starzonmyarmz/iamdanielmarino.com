0626051cf577a962dc274ac862cad05253e722c4
starzonmyarmz <starzonmyarmz@users.noreply.github.com>
2026-04-28 19:36:36 +0000

    deploy: 66cd9a48df8079165496a6e3dc95f3786b010687

--- music.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('button').forEach((el) => {
-  el.addEventListener('click', ({ currentTarget }) => {
-    const audioEl = currentTarget.querySelector('audio')
-
-    currentTarget.classList.toggle('paused', !audioEl.paused)
-    currentTarget.classList.toggle('playing', audioEl.paused)
-    audioEl.paused ? audioEl.play() : audioEl.pause()
-  })
-})
